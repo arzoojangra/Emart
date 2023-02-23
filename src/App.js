6df8379cd28557fb0b982b30e385eb4de7c300bf
arzoojangra <96476616+arzoojangra@users.noreply.github.com>
2023-02-23 15:10:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,52 +1,12 @@
-// import "./App.css";
-// import Header from "./components/class1/Header";
-// import Banner from "./components/class1/Banner";
-// import MainWebsite from "./components/class1/MainWebsite";
-// import Component1 from "./components/class2/Component1";
-// import Component2 from "./components/class2/Component2";
-// import NewComponent from "./components/class3/Component1";
-// import NewComponent2 from "./components/class3/Component2";
-// import NewComponent3 from "./components/class3/Component3";
-// import LifeCycle from "./components/class4/LifeCycle";
-// import LifeCycle2 from "./components/class4/LifeCycle2";
-// import LifeCycle3 from "./components/class4/LifeCycle3";
-// import CondRender from "./components/class5/CondRender";
-// import Forms from "./components/class5/Forms";
-// import Form2 from "./components/class5/Form2";
-// import Template1 from "./components/class6/Template1";
-// import Template2 from "./components/class6/Template2";
-// import Form from "./components/class6/Form";
-// import Template3 from "./components/class6/Template3";
-// import Template4 from "./components/class6/Template4";
+
 import Website from "./components/emart/Website";
 
-// import CartDemo from "./components/class7/CartDemo";
-// import Demo from "./components/class7/Demo";
+
 
 function App() {
   return (
     <div>
-      {/* <Header />
-      <Banner />
-      <MainWebsite />
-      <Component1 />
-      <Component2 />
-      <NewComponent />
-      <NewComponent2 />
-      <NewComponent3 />
-      <LifeCycle color="red"></LifeCycle>
-      <LifeCycle2 />
-      <LifeCycle3 />
-      <CondRender />
-      <Forms />
-      <Form2 />
-      <Template1 />
-      <Template2 />
-      <Template3 />
-      <Form />
-      <Template3 /> */}
-      {/* <CartDemo /> */}
-      {/* <Demo /> */}
+      
       <Website />
     </div>
   );
