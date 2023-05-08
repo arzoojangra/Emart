9b16707f06a37c4d6e83f9c86638b7c74b9717c7
Arzoo Jangra <aaa.jangra123@gmail.com>
2023-05-08 12:20:16 +0530

    updated home page

--- src/components/emart/Home.js
+++ src/components/emart/Home.js
@@ -1,12 +1,10 @@
 import React from "react";
 import Intro from "./Intro";
-import Products from "./Products";
 
 function Home(props) {
   return (
     <div>
       <Intro />
-      <Products />
     </div>
   );
 }
